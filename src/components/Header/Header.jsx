177e9aaa8f0fb60a42645702a886fc5458db67cf
entt0 <neks321@yandex.ru>
2021-06-12 16:41:24 +0300

    improved folder structure for folder: components/.../; also added styling and some alt for img.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import s from './Header.module.css'
 const Header = () => {
     return (
         <header className={s.header}>
-            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Free_logo.svg/440px-Free_logo.svg.png' />
+            <img alt='logo of app' src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Free_logo.svg/440px-Free_logo.svg.png' />
         </header>
     );
 }
