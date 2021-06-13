3a652a010aac3e723377f15657cc72c5abfe5b73
entt0 <neks321@yandex.ru>
2021-06-13 20:24:18 +0300

    added props for Post.jsx in files Post.jsx and MyPosts.jsx

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -9,8 +9,8 @@ let MyPosts = () => {
                 <textarea rows='4' cols='100'> Write your message here</textarea>
                 <button className={s.addPostButton}>Add Post</button>
             </div>
-            <Post />
-            <Post />
+            <Post message='My first post: "Hallo, World"'/>
+            <Post message='I want to be a programmer'/>
         </div>
     );
 }
