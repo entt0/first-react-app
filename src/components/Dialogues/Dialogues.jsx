78c7bdb050471fb5f30ccadf151885d6f421ac6d
entt0 <neks321@yandex.ru>
2021-06-29 15:13:12 +0300

    modernized dialogs

--- src/components/Dialogues/Dialogues.jsx
+++ src/components/Dialogues/Dialogues.jsx
@@ -2,8 +2,19 @@ import s from './Dialogues.module.css';
 
 const Dialogues = (props) => {
     return (
-        <div className={s.main}>
-            Dialogues will be here
+        <div className={s.dialogues}>
+            <div className={s.dialoguesItems}>
+                <div className={s.dialogue}>Nikita</div>
+                <div className={s.dialogue + ' ' + s.active}>Kris</div>
+                <div className={s.dialogue}>Vlad</div>
+                <div className={s.dialogue}>Tor</div>
+                <div className={s.dialogue}>Klim</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi,bro</div>
+                <div className={s.message}>How is your exam?</div>
+                <div className={s.message}>Kein Kommentar</div>
+            </div>
         </div>
     );
 }
