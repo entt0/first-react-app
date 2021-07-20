2e4025ac3c219d2f0ab242f1e92bf3ebcd4e7476
entt0 <neks321@yandex.ru>
2021-07-20 17:15:11 +0300

    update some code (Dialogues and MyPosts). Array was created. Lesson 24.

--- src/components/Dialogues/Dialogues.jsx
+++ src/components/Dialogues/Dialogues.jsx
@@ -16,21 +16,36 @@ const Message = (props) => {
     )
 }
 
+let dialoguesData = [
+    {id:1, name: 'Nikita'},
+    {id:2, name: 'Kris'},
+    {id:3, name: 'Vlad'},
+    {id:4, name: 'Tor'},
+    {id:5, name: 'Klim'},
+    {id:6, name: 'Dron'}
+]
+
+let messagesData = [
+    {id:1, message: 'Hi,bro'},
+    {id:2, message: 'How is your exam?'},
+    {id:3, message: 'Kein Kommentar'}
+]
+
 const Dialogues = (props) => {
     return (
         <div className={s.dialogues}>
             <div className={s.dialoguesItems}>
-                <DialogueItem name='Nikita' id='1'/>
-                <DialogueItem name='Kris' id='2'/>
-                <DialogueItem name='Vlad' id='3'/>
-                <DialogueItem name='Tor' id='4'/>
-                <DialogueItem name='Klim' id='5'/>
-                <DialogueItem name='Dron' id='6'/>
+                <DialogueItem name={dialoguesData[0].name} id={dialoguesData[0].id}/>
+                <DialogueItem name={dialoguesData[1].name} id={dialoguesData[1].id}/>
+                <DialogueItem name={dialoguesData[2].name} id={dialoguesData[2].id}/>
+                <DialogueItem name={dialoguesData[3].name} id={dialoguesData[3].id}/>
+                <DialogueItem name={dialoguesData[4].name} id={dialoguesData[4].id}/>
+                <DialogueItem name={dialoguesData[5].name} id={dialoguesData[5].id}/>
             </div>
             <div className={s.messages}>
-                <Message message='Hi,bro'/>
-                <Message message='How is your exam?'/>
-                <Message message='Kein Kommentar'/>
+                <Message message={messagesData[0].message} id={messagesData[0].id}/>
+                <Message message={messagesData[1].message} id={messagesData[1].id}/>
+                <Message message={messagesData[2].message} id={messagesData[2].id}/>
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,11 @@
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
+let postsData = [
+    {id: 1, message: 'My first post: "Hallo, World"', likes: 2},
+    {id: 2, message: 'I want to be a programmer', likes: 5}
+]
+
 let MyPosts = () => {
     return (
         <div className={s.postsWrapper}>
@@ -9,8 +14,8 @@ let MyPosts = () => {
                 <textarea rows='4' cols='100'> Write your message here</textarea>
                 <button className={s.addPostButton}>Add Post</button>
             </div>
-            <Post message='My first post: "Hallo, World"' likesCount='2'/>
-            <Post message='I want to be a programmer' likesCount='5'/>
+            <Post message={postsData[0].message} likesCount={postsData[0].likes}/>
+            <Post message={postsData[1].message} likesCount={postsData[1].likes}/>
         </div>
     );
 }
