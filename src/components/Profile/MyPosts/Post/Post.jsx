6e62d626fbb18e960958aedc7a6e644e37121bc5
entt0 <neks321@yandex.ru>
2021-11-02 19:39:33 +0300

    added hook useEffect for UserStatus.

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,15 +1,19 @@
 import s from "./Post.module.css";
 import userPhoto from './../../../../assets/Images/user.svg'
+import {useEffect, useState} from "react";
 
 let Post = (props) => {
+
+    let [count, setCount] = useState(0);
+
     return (
         <div className={s.postBlock}>
             <img alt='avatar from Sender' className={s.userAva} src={userPhoto} />
             <div className={s.postMessage}>
                 {props.message}
             </div>
-            <button className={s.likeButton}>Like It</button>
-            <span className={s.likesSpan}>{props.likesCount} likes</span>
+            <button  onClick={() => {setCount(count+1)}} className={s.likeButton}>Like It</button>
+            <span className={s.likesSpan}>{count} likes</span>
         </div>
     );
 }
